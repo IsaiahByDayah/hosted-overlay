8948d7b49546cc41dc0e1ae46518da596df9b7c7
Isaiah Smith <jordankid93@gmail.com>
2021-07-19 13:09:41 -0700

    chore: setup storybook env / decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,38 @@
+// import "typeface-nunito/index.css"
+import "bootstrap"
+
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport"
+import { MemoryRouter } from "react-router-dom"
+
+import decorator from "lib/decorator"
+
+import ThemeProvider from "components/scaffold/ThemeProvider"
+import CssBaselined from "components/scaffold/CssBaselined"
+import SnapshotStylesDecorator from "components/scaffold/SnapshotStylesDecorator"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
+    hideNoControlsWarning: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+    },
+  },
 }
+
+export const globalTypes = {}
+
+// NOTE: [Inner Most Decorator, ..., Outer Most Decorator]
+export const decorators = [
+  decorator(MemoryRouter),
+  SnapshotStylesDecorator,
+  decorator(CssBaselined),
+  decorator(ThemeProvider),
+]
