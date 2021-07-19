b09aeb1fcd1924662e75b9035230b1a8e6c6958f
Isaiah Smith <jordankid93@gmail.com>
2021-07-19 16:14:04 -0700

    chore: finish green screen

--- src/components/common/GreenScreen.tsx
+++ src/components/common/GreenScreen.tsx
@@ -1,16 +1,35 @@
-import { makeStyles } from "@material-ui/core"
+import { makeStyles, Theme } from "@material-ui/core"
 import cx from "clsx"
 
-const useStyles = makeStyles({
-  root: {},
-})
+import { calculateAspectRatioVerticalPadding } from "lib/util"
+
+type AspectRatio = string | number | (() => string | number)
+
+interface StyleProps {
+  aspectRatio: AspectRatio
+}
+
+const useStyles = makeStyles<Theme, StyleProps>(({ shape, shadows }) => ({
+  root: ({ aspectRatio }) => ({
+    backgroundColor: "#00ff00",
+    borderRadius: shape.borderRadius,
+    boxShadow: shadows[4],
+    overflow: "hidden",
+
+    width: "100%",
+    minWidth: 100,
+    height: 0,
+    paddingBottom: calculateAspectRatioVerticalPadding(aspectRatio),
+  }),
+}))
 
 export interface GreenScreenProps {
   className?: string
+  aspectRatio?: AspectRatio
 }
 
-const GreenScreen = ({ className }: GreenScreenProps) => {
-  const classes = useStyles()
+const GreenScreen = ({ className, aspectRatio = "16:9" }: GreenScreenProps) => {
+  const classes = useStyles({ aspectRatio })
 
   return <div className={cx(classes.root, className)} />
 }
