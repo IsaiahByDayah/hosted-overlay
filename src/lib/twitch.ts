397cce3d7f80db44d9057d07ceffae5c53a8ac2b
Isaiah Smith <jordankid93@gmail.com>
2021-07-26 12:08:39 -0700

    fix: twitch chat (#10)

--- src/lib/twitch.ts
+++ src/lib/twitch.ts
@@ -18,34 +18,54 @@ const onAuthenticationFailure = async (): Promise<string> => {
   return response.accessToken
 }
 
-const twitchJS = new TwitchJS({
-  token: process.env.REACT_APP_ACCESS_TOKEN,
-  clientId: process.env.REACT_APP_CLIENT_ID,
-  username: process.env.REACT_APP_USERNAME,
-  onAuthenticationFailure,
-  log: { level: "warn" },
-})
+// const onAuthenticationFailure = async () =>
+//   fetchUtil("https://id.twitch.tv/oauth2/token", {
+//     method: "post",
+//     search: {
+//       grant_type: "refresh_token",
+//       refresh_token: process.env.REACT_APP_REFRESH_TOKEN,
+//       client_id: process.env.REACT_APP_CLIENT_ID,
+//       client_secret: process.env.REACT_APP_CLIENT_SECRET,
+//     },
+//   }).then((response: any) => response.accessToken)
 
-export const chat = twitchJS.chat
+let _twitch: TwitchJS
+const getTwitch = () => {
+  if (_twitch) {
+    return _twitch
+  } else {
+    _twitch = new TwitchJS({
+      token: process.env.REACT_APP_ACCESS_TOKEN,
+      clientId: process.env.REACT_APP_CLIENT_ID,
+      username: process.env.REACT_APP_USERNAME,
+      onAuthenticationFailure,
+      log: { level: "warn" },
+    })
+    return _twitch
+  }
+}
 
-export const api = twitchJS.api
+// export const getChat = () => getTwitch().chat
+// const getApi = () => getTwitch().api
 
 export const getFollowerCount = async (): Promise<number> => {
-  const res = await api.get("users/follows", {
+  const res = await getTwitch().api.get("users/follows", {
     search: {
       to_id: process.env.REACT_APP_USER_ID,
     },
   })
   console.log("Response: ", res)
   return res.total
+  // return 69
 }
 
 export const getSubscriberCount = async (): Promise<number> => {
-  const res = await api.get("subscriptions", {
+  const res = await getTwitch().api.get("subscriptions", {
     search: {
       broadcaster_id: process.env.REACT_APP_USER_ID,
     },
   })
   console.log("Response: ", res)
   return res.total
+  // return 69
 }
