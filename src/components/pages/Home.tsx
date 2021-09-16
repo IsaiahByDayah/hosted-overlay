44f53dca4db6fcc177ee74107ad433c0ac9d1ef9
Isaiah Smith <6565265+IsaiahByDayah@users.noreply.github.com>
2021-09-16 13:59:54 -0700

    chore: simple home page ui update

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,45 @@
 import { useState } from "react"
-import { TextField, Button } from "@material-ui/core"
+import {
+  makeStyles,
+  TextField,
+  Button,
+  Box,
+  Container,
+  Typography,
+} from "@material-ui/core"
 import { signInWithEmailAndPassword } from "firebase/auth"
 
 import firebase from "lib/firebase"
 
+import Header from "components/common/Header"
+
+const useStyles = makeStyles(({ spacing }) => ({
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    gap: spacing(4),
+  },
+  pageTitle: {
+    fontWeight: "bold",
+  },
+  section: {
+    display: "flex",
+    flexDirection: "column",
+    gap: spacing(2),
+  },
+  sectionTitle: {
+    fontWeight: "bold",
+  },
+  row: {
+    gap: spacing(2),
+  },
+  noShrink: {
+    flexShrink: 0,
+  },
+}))
+
 const Home = () => {
+  const classes = useStyles()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
@@ -14,22 +49,37 @@ const Home = () => {
 
   return (
     <div>
-      <TextField
-        placeholder="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.currentTarget.value)
-        }}
-      />
-      <TextField
-        placeholder="password"
-        type="password"
-        value={password}
-        onChange={(e) => {
-          setPassword(e.currentTarget.value)
-        }}
-      />
-      <Button onClick={() => submit()}>Sign In</Button>
+      <Header />
+      <Container className={classes.content} component={Box} mt={2}>
+        {/* <Typography className={classes.pageTitle} variant="h5">
+          Home Page
+        </Typography> */}
+
+        {/* Signin Section */}
+        <div className={classes.section}>
+          <Typography className={classes.sectionTitle}>
+            Sign In / Sign Up
+          </Typography>
+          <Box className={classes.row} display="flex" alignItems="start">
+            <TextField
+              placeholder="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.currentTarget.value)
+              }}
+            />
+            <TextField
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.currentTarget.value)
+              }}
+            />
+            <Button onClick={() => submit()}>Sign In</Button>
+          </Box>
+        </div>
+      </Container>
     </div>
   )
 }
