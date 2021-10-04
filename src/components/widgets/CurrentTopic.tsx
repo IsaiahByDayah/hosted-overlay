d1704d95651cb521d073eea91d2f3f53b388920b
Isaiah Smith <6565265+IsaiahByDayah@users.noreply.github.com>
2021-10-04 14:29:48 -0700

    Chore: update StatusBar styles

--- src/components/widgets/CurrentTopic.tsx
+++ src/components/widgets/CurrentTopic.tsx
@@ -1,40 +1,28 @@
-import { Typography, Paper } from "@mui/material";
-
-import makeStyles from '@mui/styles/makeStyles';
+import { Box, Typography, Paper } from "@mui/material"
+import { SxProps, Theme } from "@mui/system"
 
 import { useOverlayContext } from "components/scaffold/OverlayProvider"
 
-const useStyles = makeStyles(({ spacing }) => ({
-  label: {
-    fontWeight: 900,
-    marginBottom: spacing(1.5),
-  },
-  paper: {
-    padding: spacing(1, 2),
-  },
-  topic: {
-    fontWeight: 700,
-  },
-}))
-
 export interface CurrentTopicProps {
-  className?: string
+  sx?: SxProps<Theme> | undefined
 }
-const CurrentTopic = ({ className }: CurrentTopicProps) => {
-  const classes = useStyles()
+const CurrentTopic = ({ sx }: CurrentTopicProps) => {
   const { overlay } = useOverlayContext()
 
   return (
-    <div className={className}>
-      <Typography className={classes.label} align="center" variant="h5">
-        Current Topic
-      </Typography>
-      <Paper className={classes.paper}>
-        <Typography className={classes.topic} align="center" variant="h6">
+    <Box sx={sx}>
+      <Typography fontWeight={900}>Currently...</Typography>
+      <Paper
+        sx={{
+          py: 1,
+          px: 2,
+        }}
+      >
+        <Typography align="center" variant="h6">
           {overlay?.currentTopic ?? "Having fun!"}
         </Typography>
       </Paper>
-    </div>
+    </Box>
   )
 }
 
