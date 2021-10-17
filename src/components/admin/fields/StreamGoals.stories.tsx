f2667525f1afea0b9547e156b1f9ce00c5f08f78
Isaiah Smith <6565265+IsaiahByDayah@users.noreply.github.com>
2021-10-16 20:02:37 -0700

    chore: storyshots

--- src/components/admin/fields/StreamGoals.stories.tsx
+++ src/components/admin/fields/StreamGoals.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta, Story } from "@storybook/react"
 
-import StreamCounts from "components/admin/fields/StreamCounts"
+import StreamGoals from "components/admin/fields/StreamGoals"
 
 export default {
-  title: "Admin/Fields/Stream Counts",
-  component: StreamCounts,
+  title: "Admin/Fields/Stream Goals",
+  component: StreamGoals,
 } as Meta
 
-export const Basic: Story = (args) => <StreamCounts {...args} />
+export const Basic: Story = (args) => <StreamGoals {...args} />
